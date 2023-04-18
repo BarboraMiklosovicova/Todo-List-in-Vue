e4603672fa01f81920135e73eb3e88808e6881ee
Barbora <miklosovicovab@gmail.com>
2023-04-18 18:41:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import ShowTodo from "./components/ShowTodo.vue";
+import { Todo } from "./models/Todo";
+</script>
+
+<template>
+  <ShowTodo></ShowTodo>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/ShowTodo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Todo } from "../models/Todo";
+
+interface IShowTodo {
+  todo: Todo;
+}
+
+defineProps<IShowTodo>(); // ta emot todo från parent
+</script>
+
+<template>
+  <input type="text" placeholder="Add task" />
+  <button>Add</button>
+</template>
+
+<style scoped></style>
